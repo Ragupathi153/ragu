8cd932da62f0bcc5c21ae787ccf126a368c3b78a
Ragupathi153 <32413458+Ragupathi153@users.noreply.github.com>
2017-09-30 05:50:02 -0700

    ragu2.c

---
+++ ragu2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter amy number");
+scanf("%d",n);
+if(n%2==0)
+{
+printf("entered number is even");
+}
+else
+{
+printf("entered number is odd");
+}
+}
