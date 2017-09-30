f401d62cc02bafb91e1696b0e397a925197eac14
Ragupathi153 <32413458+Ragupathi153@users.noreply.github.com>
2017-09-30 05:24:15 -0700

    Create ragu.c

---
+++ ragu.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter any interger");
+scanf("%d",n);
+if(n<=0)
+{
+if(n==0)
+{
+printf("entered number is zero");
+}
+else
+{
+printf("entered number is negative");
+}
+else
+{
+printf("entered number is positive");
+}
+}
