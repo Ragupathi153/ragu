e0a9ba8fc08561d316ceed05130bb66d29205da6
Ragupathi153 <32413458+Ragupathi153@users.noreply.github.com>
2017-10-02 05:14:30 -0700

    Create ragu15.c

---
+++ ragu15.c
@@ -0,0 +1,15 @@
+#include<stdi.h>
+#include<conio.h>
+void main()
+{
+int i,f,l;
+printf("enter the first and last interval");
+scanf("%d%d",&f,&l);
+for(i=f;i<=l;i++)
+{
+if(i%2==0)
+{
+printf("%d is an even number",i);
+}
+}
+}
