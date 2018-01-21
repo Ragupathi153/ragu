32081b3d1bd860b40e271ff5c128824a64a9e582
Ragupathi153 <32413458+Ragupathi153@users.noreply.github.com>
2018-01-21 10:13:40 +0530

    Create primeitl.c

---
+++ primeitl.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+int l,h,i;
+int flag=0;
+printf("enter the intervals");
+scanf("%d%d",l,h);
+printf("the prime numbers between the intervals are:");
+while(l<h)
+{
+for(i=2;i<=l/2;i++)
+{
+if(l%i==o)
+{
+flag=1;
+}
+}
+if(flag==0)
+{
+printf("%d",l);
+++low
+}
+}
+}
