648fdebc15fab4458a0db3aabd96cbc62e2590e6
Ragupathi153 <32413458+Ragupathi153@users.noreply.github.com>
2018-01-24 15:36:49 +0530

    Create primeinterval.c

---
+++ primeinterval.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+int l,h,i;
+int f=0;
+printf("enter the intervals");
+scanf("%d%d",&l,&h);
+printf("the prime numbers between the intervals are:");
+while(l<h)
+{
+for(i=2;i<=l/2;i++)
+{
+if(l%i==o)
+{
+f=1;
+}
+}
+if(f==0)
+{
+printf("%d",l);
+++low
+}
+}
+}
