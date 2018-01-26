5e25a7be119023386d6a384f3a08a488866b2901
Ragupathi153 <32413458+Ragupathi153@users.noreply.github.com>
2018-01-26 15:27:28 +0530

    Create prgm43.c

---
+++ prgm43.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+	char a[20];
+	char b[20];
+	printf("enter the character:\n");
+	scanf("%s",&a);
+	printf("enter the next character:\n");
+	scanf("%s",&b);
+	printf("%s%s",a,b);
+}
